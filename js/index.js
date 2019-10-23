d2ddb82caf2bfa217471c3c2932e53e6e91a8155
sania1905555 <sania1905555@gmail.com>
2019-10-23 10:06:26 +0000

    coment3

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-ыва
-dfg
-fdgdfg
-dfg
+document.querySelector('.page-loaded')
+.innerText = new Date().toLocaleTimeString();
+document.querySelector('.ajax-get-html')
+.addEventListener('click', getAjaxHtml);
+function getAjaxHtml() {
+    const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+if (xhr.readyState === 4 && xhr.status === 200){
+document.querySelector('.html-container')
+.innerHTML = xhr.responseText;
+}
+
+}
+xhr.open('GET', 'client-date.html', true);
+xhr.send();
+}
