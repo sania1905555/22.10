c7d13032a4e58dc9df808c9cee7e62f4762919e3
sania1905555 <sania1905555@gmail.com>
2019-10-23 12:18:31 +0000

    coment3

--- js/index.js
+++ js/index.js
@@ -15,3 +15,47 @@ document.querySelector('.html-container')
 xhr.open('GET', 'client-date.html', true);
 xhr.send();
 }
+document.querySelector('.fetch-html')
+.addEventListener('click', fetchHtml);
+function fetchHtml() {
+fetch('client-date.html')
+.then( response => response.text() )
+.then (html => document.querySelector('.html-container')
+.innerHTML = html);
+}
+
+//JSON
+
+document.querySelector('.ajax-get-json')
+.addEventListener('click', getAjaxJson);
+
+function getAjaxJson() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200){
+            const clientData = JSON.parse(xhr.responseText);
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
+        }
+
+    }
+xhr.open('GET', 'client-data.json', true);
+xhr.send();
+}
+
+
+
+// fetch
+
+
+document.querySelector('.fetch-json')
+.addEventListener('click', fetchJson);
+function fetchJson() {
+fetch('client-data.json')
+.then( response => response.json() )
+.then (clientData => {
+        document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
+        }
+        );
+}
